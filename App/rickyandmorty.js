5a2eba09d550504b1a80a0c2bce9577fd15a4a6c
polero2002 <poleropvp@hotmail.com>
2025-05-14 17:44:18 -0600

    Funcion asincrona para obtener los personajes de la api

--- App/rickyandmorty.js
+++ App/rickyandmorty.js
@@ -1,5 +1,22 @@
 //Prueba Tecnica Auronix para intership
+//Autor: Omar Yair Arellano Polo
 
 //URL BASE DE LA API ORIGINAL
 const API_URL = "https://rickandmortyapi.com/api/character";
+
+//Funcion asincrona para obtener los datos de los personajes
+async function getcharacters() {
+  const response = await fetch(API_URL);
+
+  if (!response.ok) {
+    throw new Error("fetch error");
+  }
+
+  const data = await response.json();
+  //Linea para probar en consola si funciona
+  //console.log(data.results);
+
+  return data.results;
+}
+
 
