6f15ede03ef3f036a0bd344c6451029f7e323fe9
polero2002 <poleropvp@hotmail.com>
2025-05-14 20:26:22 -0600

    importamos el framework de express, importamos la funcion de main del otro archivo e inicializamos la app de express

---
+++ App/index.js
@@ -0,0 +1,8 @@
+// Importamos el framework Express para crear el servidor
+const express = require("express");
+
+// Importamos la función main
+const { main } = require("./rickyandmorty");
+
+// Inicializamos la aplicación de Express
+const app = express();
