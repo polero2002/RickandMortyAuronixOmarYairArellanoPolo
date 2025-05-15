77d503f9af86248af7dac40ec117594e38204ca4
polero2002 <poleropvp@hotmail.com>
2025-05-14 20:32:47 -0600

    definimos un endpoint  /characters el cual se encarga de llamar a la funcion main y devolver en json la respuesta

--- App/index.js
+++ App/index.js
@@ -9,3 +9,16 @@ const app = express();
 
 // Se define el puerto donde se va a escuchar el server
 const PORT = 3000;
+// definimos el endpoint /personajes con el metodo get para acceder a los personajes filtrados y formateados
+app.get("/personajes", async (res) => {
+  try {
+    // llamamos a la funcion main
+    const data = await main();
+
+    // en la respuesta damos los datos con json
+    res.json(data);
+  } catch (error) {
+    // En caso de error, devolvemos un mensaje de error con código 500.
+    res.status(500).json({ error: error.message });
+  }
+});
